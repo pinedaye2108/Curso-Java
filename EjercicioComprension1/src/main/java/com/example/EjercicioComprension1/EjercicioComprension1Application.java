61973f09b53f855bb28a46244f15d0d7ca2437ca
pinedaye2108 <pinedaye2108@gmail.com>
2025-10-23 09:51:29 -0400

    Ejercicio de Comprension 1

---
+++ EjercicioComprension1/src/main/java/com/example/EjercicioComprension1/EjercicioComprension1Application.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioComprension1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioComprension1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioComprension1Application.class, args);
+	}
+
+}
---
+++ EjercicioComprension1/src/test/java/com/example/EjercicioComprension1/EjercicioComprension1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EjercicioComprension1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioComprension1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
